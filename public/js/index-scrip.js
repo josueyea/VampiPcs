9f1d926ae34f1f0b8aa5d498b5ad3f4e7c55205e
josueyea <apolinarioelman@gmail.com>
2025-05-30 04:24:49 -0500

    Prueba de css

--- public/js/index-scrip.js
+++ public/js/index-scrip.js
@@ -6,6 +6,7 @@
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
